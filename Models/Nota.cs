213babb0f8d958e9f8fc14619fdef1b37502c457
MatiasM <matias_monasterio@outlook.com>
2020-11-04 21:59:17 -0300

    correccion sintaxis require por required

--- Models/Nota.cs
+++ Models/Nota.cs
@@ -1,23 +1,22 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore;
 
 namespace  Notas.Models
 {
     public  class Nota
   {
 
-    // [Key]
-    // [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public int ID {get; set;}
+    [Key]
+    [DatabaseGenerated( DatabaseGeneratedOption.Identity )]
+    public int ID { get; set; }
 
-    // [Require]
-    public string Titulo {get; set;}
+    [Required]
+    public string Titulo { get; set; }
 
-    // [Require]
-    public string Cuerpo {get; set;}
+    [Required]
+    public string Cuerpo { get; set; }
     
-    // [Require]
-    public Usuario  Creador {get; set;}
+    [Required]
+    public Usuario  Creador { get; set; }
    }
 }
--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -8,12 +8,12 @@ namespace Notas.Models
 
     public class Usuario  
   {
-    // [Key]
-    public string Mail {get;set;} 
+    [Key]
+    public string Mail { get;set; } 
 
-    // [Require]
-    public String Nombre {get;set;}
+    [Required]
+    public String Nombre { get;set; }
 
-    public List<Nota> Notas {get;set;}
+    public List<Nota> Notas { get;set; }
   }
 }
