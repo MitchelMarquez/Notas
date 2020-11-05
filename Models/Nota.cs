47d6264436bb36d7054c7fc4f2283c1191959511
MatiasM <matias_monasterio@outlook.com>
2020-11-04 21:18:24 -0300

    Definicion propiedades bd

--- Models/Nota.cs
+++ Models/Nota.cs
@@ -3,12 +3,17 @@ namespace  Notas.Models
     public  class Nota
   {
 
+    [Key]
+    [DatabaseGenerated(DataBaseGeneratedOption.Identity)]
     public int ID {get; set;}
 
+    [Require]
     public string Titulo {get; set;}
 
+    [Require]
     public string Cuerpo {get; set;}
     
+    [Require]
     public Usuario  Creador {get; set;}
    }
 }
--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -6,11 +6,12 @@ namespace Notas.Models
 
     public class Usuario  
   {
+    [Key]
+    public string Mail {get;set;} 
 
-   public string Mail {get;set;} 
+    [Require]
+    public String Nombre {get;set;}
 
-   public String Nombre {get;set;}
-
-   public List<Nota> Notas {get;set;}
+    public List<Nota> Notas {get;set;}
   }
 }
