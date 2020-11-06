87627be9df40923ff5ff2b9489354b7c1f9bf17c
MatiasM <matias_monasterio@outlook.com>
2020-11-06 02:16:36 -0300

    add form html css and js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+showNotaForm.addEventListener('click', closeFormEvent );
+closeForm.addEventListener('click', closeFormEvent )
+
+function closeFormEvent(e){
+    e.preventDefault();
+    notaForm.classList.toggle("showForm");
+}
